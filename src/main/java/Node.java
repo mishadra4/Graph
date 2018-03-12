ae0486790d11aa7b40b471fa3c3475a0fc84e61c
mishadra4 <qwewerty12@gmail.com>
2018-03-13 01:25:08 +0200

    Graph fourth Misha`s commit

--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -24,4 +24,12 @@ public class Node {
     public void setNodeInfo(String nodeInfo) {
         this.nodeInfo = nodeInfo;
     }
+
+    public Integer getSequenceNumber() {
+        return sequenceNumber;
+    }
+
+    public void setSequenceNumber(Integer sequenceNumber) {
+        this.sequenceNumber = sequenceNumber;
+    }
 }
