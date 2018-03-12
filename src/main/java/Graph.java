6a83931d57a787534b874795058962248579751f
mishadra4 <qwewerty12@gmail.com>
2018-03-12 23:33:01 +0200

    Graph second commit

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -1,11 +1,58 @@
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+
 
 public class Graph  {
     List<List<Integer>> matrix = new ArrayList<>();
     List<Node> nodeList = new ArrayList<>();
 
+    public Graph(List<List<Integer>> matrix) {
+        this.matrix = matrix;
+        for (int i = 0; i < matrix.size(); i++) {
+            nodeList.add(new Node(i+1));
+        }
+    }
 
+    public void BFS(){
+        Queue<Integer> nodeQueue = new PriorityQueue<>();
+        int currentNode = 0;
+        nodeList.get(currentNode).setNodeStatus(NodeStatus.GREY);
+        nodeQueue.add(currentNode);
+        int vertex;
+
+
+        System.out.println(nodeList.get(currentNode).getNodeInfo());
+
+        /*while(!nodeQueue.isEmpty())
+        {
+            int current = nodeQueue.remove();
+            while((vertex = getSuccessor(current)) != -1)
+            {
+                nodeList.get(vertex).setNodeStatus(NodeStatus.GREY);
+                nodeQueue.add(vertex);
+                System.out.println(nodeList.get(vertex).getNodeInfo());
+            }
+
+        }*/
+
+        for (Node node : nodeList) {
+            node.setNodeStatus(NodeStatus.BLACK);
+        }
+    }
+
+    public List<List<Integer>> getMatrix() {
+        return matrix;
+    }
+
+    public void setMatrix(List<List<Integer>> matrix) {
+        this.matrix = matrix;
+    }
+
+    public List<Node> getNodeList() {
+        return nodeList;
+    }
+
+    public void setNodeList(List<Node> nodeList) {
+        this.nodeList = nodeList;
+    }
 }
+
--- src/main/java/Node.java
+++ src/main/java/Node.java
@@ -1,4 +1,25 @@
 public class Node {
-    NodeStatus nodeStatus = NodeStatus.BLACK;
+    NodeStatus nodeStatus;
+    String nodeInfo = "I`m node ¹";
 
+    public Node(int orderNumber) {
+        nodeStatus = NodeStatus.BLACK;
+        this.nodeInfo += orderNumber;
+    }
+
+    public NodeStatus getNodeStatus() {
+        return nodeStatus;
+    }
+
+    public void setNodeStatus(NodeStatus nodeStatus) {
+        this.nodeStatus = nodeStatus;
+    }
+
+    public String getNodeInfo() {
+        return nodeInfo;
+    }
+
+    public void setNodeInfo(String nodeInfo) {
+        this.nodeInfo = nodeInfo;
+    }
 }
