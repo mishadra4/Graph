32566ed541f0b20eea1868879fd288a41b866582
Marta Klachko <marta.klachko@gmail.com>
2018-03-13 11:06:00 +0200

    added removeNode method

--- src/main/java/Graph.java
+++ src/main/java/Graph.java
@@ -8,20 +8,20 @@ public class Graph implements Comparable<Graph> {
     public Graph(List<List<Integer>> matrix) {
         this.matrix = matrix;
         for (int i = 0; i < matrix.size(); i++) {
-            nodeList.add(new Node(i+1));
+            nodeList.add(new Node(i + 1));
         }
     }
 
-    public void addPath(Integer node1,Integer node2, Integer weight){
-        matrix.get(node1-1).set(node2-1,weight);
+    public void addPath(Integer node1, Integer node2, Integer weight) {
+        matrix.get(node1 - 1).set(node2 - 1, weight);
     }
-    
-    public void removePath(Integer node1,Integer node2){
+
+    public void removePath(Integer node1, Integer node2) {
         matrix.get(node1).set(node2, 0);
     }
 
 
-    public List<Node> breadthFirstSearch(){
+    public List<Node> breadthFirstSearch() {
         List<Node> result = new ArrayList<>(); // result list returned by method
         Queue<Integer> nodeQueue = new PriorityQueue<>();
         int currentNode = 0;
@@ -29,12 +29,12 @@ public class Graph implements Comparable<Graph> {
         nodeQueue.add(currentNode);
         result.add(nodeList.get(currentNode));//Open first node and push it to queue
 
-        while(!nodeQueue.isEmpty()) { //cycle discovers all neighbour nodes
+        while (!nodeQueue.isEmpty()) { //cycle discovers all neighbour nodes
             currentNode = nodeQueue.remove();
-            for (int i = 0; i < nodeList.size(); i++){ //work out all ways which are going from current queue node
-                if(matrix.get(currentNode).get(i) != 0 && nodeList.get(i).getNodeStatus() == NodeStatus.BLACK){
+            for (int i = 0; i < nodeList.size(); i++) { //work out all ways which are going from current queue node
+                if (matrix.get(currentNode).get(i) != 0 && nodeList.get(i).getNodeStatus() == NodeStatus.BLACK) {
                     nodeList.get(i).setNodeStatus(NodeStatus.GREY);
-                    if(nodeList.get(i).getNodeStatus() != NodeStatus.WHITE) {
+                    if (nodeList.get(i).getNodeStatus() != NodeStatus.WHITE) {
                         nodeQueue.add(i);
                     }
                     result.add(nodeList.get(i));
@@ -49,7 +49,7 @@ public class Graph implements Comparable<Graph> {
         return result;
     }
 
-    public List<Node> depthFirstSearch(){
+    public List<Node> depthFirstSearch() {
         List<Node> result = new ArrayList<>();
         int currentNode = 0;
         Queue<Integer> nodeStack = new ArrayDeque();
@@ -60,12 +60,12 @@ public class Graph implements Comparable<Graph> {
         result.add(nodeList.get(currentNode));
 
 
-        while(!nodeStack.isEmpty()) //cycle discovers all neighbour nodes
+        while (!nodeStack.isEmpty()) //cycle discovers all neighbour nodes
         {
             currentNode = nodeStack.peek();
-            for (int i = 0; i < nodeList.size(); i++){ //work out all ways which are going from current queue node
-                if(matrix.get(currentNode).get(i) != 0 && nodeList.get(i).getNodeStatus() == NodeStatus.BLACK){
-                    if(nodeList.get(i).getNodeStatus() == NodeStatus.BLACK) {
+            for (int i = 0; i < nodeList.size(); i++) { //work out all ways which are going from current queue node
+                if (matrix.get(currentNode).get(i) != 0 && nodeList.get(i).getNodeStatus() == NodeStatus.BLACK) {
+                    if (nodeList.get(i).getNodeStatus() == NodeStatus.BLACK) {
                         nodeStack.add(i);
                         //nodeList.get(i).setNodeStatus(NodeStatus.WHITE);
                     }
@@ -109,10 +109,10 @@ public class Graph implements Comparable<Graph> {
 
     @Override
     public int compareTo(Graph o) {
-        if(this.getNodeList().size() > o.getNodeList().size()){
+        if (this.getNodeList().size() > o.getNodeList().size()) {
             return 1;
         }
-        if(this.getNodeList().size() < o.getNodeList().size()){
+        if (this.getNodeList().size() < o.getNodeList().size()) {
             return -1;
         }
         return 0;
@@ -145,6 +145,20 @@ public class Graph implements Comparable<Graph> {
 
     }
 
+    /**
+     * remove node to the graph with its paths.
+     */
+    public boolean removeNode(Integer node) {
+
+        if (node >= nodeList.size()) {
+            System.out.println("node doesnt exist.");
+            return false;
+        }
+
+        nodeList.remove(node - 1);
+        matrix.remove(node - 1);
+        return true;
+    }
 
 }
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,22 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         List<List<Integer>> matrix = new ArrayList<>();
-        matrix.add(Arrays.asList(0,1,0,0));
-        matrix.add(Arrays.asList(0,0,0,1));
-        matrix.add(Arrays.asList(1,0,0,1));
-        matrix.add(Arrays.asList(1,0,1,0));
+        matrix.add(Arrays.asList(0, 1, 0, 0));
+        matrix.add(Arrays.asList(0, 0, 0, 1));
+        matrix.add(Arrays.asList(1, 0, 0, 1));
+        matrix.add(Arrays.asList(1, 0, 1, 0));
         Graph graph = new Graph(matrix);
 
         List<Node> foundNodes = graph.breadthFirstSearch();
         System.out.println(foundNodes);
         graph.breadthFirstSearch();
         Map<Integer, Integer> paths = new HashMap<>();
-        paths.put(2,3);
+        paths.put(2, 3);
         graph.addNode(5, paths);
 
         System.out.println(graph.matrix);
-        //graph.depthFirstSearch();
+        graph.removeNode(3);
+        System.out.println(graph.matrix);
+
     }
 }
